0beabdd6ac2cd28b2e392baca1a9e5378f92f317
Dunnoice <22249898+Dunnoice@users.noreply.github.com>
2020-06-10 13:17:44 +1000

    Add index
    Remove Demo
    Add AppBundle
    Problem index's css

---
+++ src/Shop/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shop\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+/*	public function indexAction($name)
+	{
+		return $this->render('ShopAppBundle:Default:index.html.twig', ['name' => $name]);
+	}*/
+}
